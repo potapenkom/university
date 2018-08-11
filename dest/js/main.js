822251766433edf7b9a5ea9ba87442041727466c
potapenkom <potapenko05121985@gmail.com>
2018-08-11 13:41:07 +0300

    new carousel

--- dest/js/main.js
+++ dest/js/main.js
@@ -1,4 +1,26 @@
-$('.js-carousel').slick({
-    nextArrow: '.review__next',
-    prevArrow: '.review__prev'
-  });
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i = 1;
+    var slides = document.getElementsByClassName("siide__js");
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none"; 
+    }
+    slides[slideIndex-1].style.display = "block";
+}
