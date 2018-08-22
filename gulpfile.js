4f9a97739374a0a09c37d0f862da52493f9e7f19
potapenkom <potapenko05121985@gmail.com>
2018-08-23 01:21:38 +0300

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,35 @@ var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
-
+var browserSync = require('browser-sync').create();
 
 var plugins = [
 	autoprefixer({browsers: ['last 2 version']}),
 	cssnano()
 ]
- 
+
+
 gulp.task('sass', function () {
 	return gulp.src('src/sass/**/*.sass')
         .pipe(sass())
     	.pipe(postcss(plugins))
-    	.pipe(gulp.dest('dest/css'));
+		.pipe(gulp.dest('dest/css')); 
 });
 
-gulp.task('watch', function() {
+gulp.task('browser-sync', function() { 
+    browserSync({ 
+        server: { 
+            baseDir: 'dest' 
+        },
+        notify: false 
+    });
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], function() {
     gulp.watch('src/sass/**/*.sass', ['sass']); 
     
 });
+
+
+
 
